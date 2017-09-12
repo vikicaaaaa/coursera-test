5a9cd2769dcf0c2067887693bcbaea7252a7d7a3
Viktorija <viki@coursera.com>
2017-09-12 13:47:11 +0200

    Version2

---
+++ app.js
@@ -0,0 +1,38 @@
+//kostur
+(function (){
+  'use strict';
+
+/*
+prvi zad
+angular.module('myFirstApp', []) //ide u html
+  //special object  $
+  .controller('myFirstController', function($scope){
+    $scope.name = "Viktorija";
+    $scope.sayHello=function(){
+      return "Hello";
+    };
+  });*/
+  /*
+angular.module('NameCalculator', []) //ide u body
+
+.controller('NameCalculatorController', function($scope){
+$scope.name="";
+  $scope.total = 0 ;
+
+  $scope.display=function () {
+    var totalNameVal=calculating($scope.name);
+    $scope.total=totalNameVal;
+  };
+
+  function calculating(string){
+    var totalString=0;
+    for(i=0; i< string.length; i++){
+      totalString += string.charCodeAt(i);
+      }
+      return totalString;
+    }
+});*/
+//treci zadatak
+
+
+})();
